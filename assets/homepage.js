f6dd587ebf3c610123b7a4b6fb9165f7e205a1f4
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-12 13:59:37 -0700

    mycharts testing

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
 })
+const ctx = document.getElementById('myChart');
+      
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
