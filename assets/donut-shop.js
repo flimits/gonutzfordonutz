892fb1f6b2ae741e6c57a155fe9cc509a63eb865
Jason Savoie <flimits@gmail.com>
2023-10-12 15:01:57 -0700

    adding ability to blog on one box

--- assets/donut-shop.js
+++ assets/donut-shop.js
@@ -4,10 +4,19 @@ var todayDate = dayjs().format('MMMM D, YYYY')
 console.log("date " + todayDate);
 getTodaysDate.textContent = todayDate;
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-})
-// get some infput for the perchange to advertise in their blog.
-$(document).ready(function() {
     M.updateTextFields();
-  });
+
+})
+
+// Here the value is stored for Merchant Blog  
+// function myFunction() {
+//     var m = document.getElementById("myStore").value;
+//     document.getElementById("saveItM").innerHTML = m;
+// } 
+
+$("#btn").on("click", function() {
+    var newData = $("#myStore").val();
+    $("#saveItM").append("<p>" + newData + "</p>");
+})
