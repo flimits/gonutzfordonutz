747b9fc2f70d55c47d4a88038444506efd62b3ec
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-11 17:04:45 -0700

    playing with mobile nav functionality

--- assets/donut-locator.js
+++
@@ -1,9 +0,0 @@
-// var donutMapURL = "https://www.google.com/maps/embed/v1/search?key=AIzaSyA2Qq9tiWUtSdlkiBJov0EMgRDPTEMKJHw&q=donut+shops+in+United+States"
-
-// fetch(donutMapURL)
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(data){
-//     console.log(data);
-// })
