24776a0aaf6e2ab5d84557e6d20131c7650b823e
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-13 01:10:00 -0700

    added working search bar for map

--- assets/donut-locator.js
+++ assets/donut-locator.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
 })
+
+$(".btn").on("click",function(event){
+    var location = $("#textarea1").val();
+    event.preventDefault();
+    $("iframe.map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyA2Qq9tiWUtSdlkiBJov0EMgRDPTEMKJHw&zoom=7&q=donut+shops+in+" + location);
+})
