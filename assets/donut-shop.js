f964e863823fbd22d966302dfb6dc0ac49e3d936
Jason Savoie <flimits@gmail.com>
2023-10-12 22:31:54 -0700

    Evened out both sides to have similar inputs

--- assets/donut-shop.js
+++ assets/donut-shop.js
@@ -1,6 +1,6 @@
-var getTodaysDate = document.querySelector(".date")
+var getTodaysDate = document.querySelector(".date");
 
-var todayDate = dayjs().format('MMMM D, YYYY')
+var todayDate = dayjs().format('MMMM D, YYYY');
 getTodaysDate.textContent = todayDate;
 
 $(document).ready(function () {
