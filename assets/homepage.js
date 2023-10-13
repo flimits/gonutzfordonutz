af49fb66cfc43b4f38e272a9bdd929a327c25bdd
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-12 17:48:25 -0700

    Jack/chart testing (#34)
    * fixed js filepaths on all pages
    
    * added footer card to homepage
    
    * homepage header added with image
    
    * added mobile menu hamburger (lol)
    
    * mycharts testing
    
    * testing chart js functionality

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
 })
+var donutPoll = document.getElementById('myChart');
+      
+new Chart(donutPoll, {
+  type: 'doughnut',
+  data: {
+    labels: ['Donut','Doughnut'],
+    datasets: [{
+      label: '# of Votes',
+      data: [14,2],
+    }]
+  },
+  options: {
+
+  },
+  plugins: []
+});
