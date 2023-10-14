32a86f0c8db9faeb6d4538c01c9040b05c9b9d9a
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-14 16:50:17 -0700

    API call testing

--- assets/recipes.js
+++ assets/recipes.js
@@ -48,3 +48,31 @@ $(".btn-large").on("click",function(){
         }
     }
 });
+
+$("#recipe-btn").on("click", function(event){
+    event.preventDefault();
+    
+    var query = $("#textarea1").val()
+    var donutSearch = "https://api.spoonacular.com/recipes/complexSearch?apiKey=32c66c99f2d24929ba5b950ee9a6ad04&query="+ query;
+
+    fetch(donutSearch)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        console.log(data.results);
+        $(data.results).each(function(){
+            console.log(this.id);
+        })
+        var recipeID = data.results[0].id
+        var recipeURL = "https://api.spoonacular.com/recipes/" + recipeID + "/information?apiKey=32c66c99f2d24929ba5b950ee9a6ad04"
+        fetch(recipeURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+    })
+})
